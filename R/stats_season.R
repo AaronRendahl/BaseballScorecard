fd114d3f9d41cb78ed8c73e1853d1a0fc445ea09
Aaron Rendahl <rend0020@umn.edu>
2024-03-20 20:03:03 -0500

    ok, think everything is fixed again now

--- R/stats_season.R
+++ R/stats_season.R
@@ -1,5 +1,5 @@
 all_stats <- function(games, team) {
-  the_game_stats <- games |> select(stats) |> unnest(stats)
+  the_game_stats <- gs |> select(code, game) |> unnest(game)
   b.all <- the_game_stats |> filter(Team==team) |> select(Batter_Stats) |> unnest(Batter_Stats) |>
     mutate(G=1) |>
     group_by(Number, Name) |> summarize(across(everything(), sum)) |> ungroup() |>
@@ -16,9 +16,9 @@ all_stats <- function(games, team) {
 
 get_all_stats <- function(gs, team) {
   all_the_stats <- all_stats(gs, team=team)
-  the_game_stats <- gs |> select(about, when, code, stats, any_of('scorecard_link')) |> unnest(stats)
+  the_game_stats <- gs |> select(about, when, code, game, any_of('scorecard_link')) |> unnest(game)
   batting_stats <- the_game_stats |> filter(Team==team) |>
-    select(about, when, any_of('scorecard_link'), vs, Batter_Stats) |> unnest(Batter_Stats) |>
+    select(about, when, any_of('scorecard_link'), Batter_Stats) |> unnest(Batter_Stats) |>
     mutate(type="Batting") |>
     bind_rows(all_the_stats$Batting %>% mutate(type="Batting", about="Season")) %>%
     mutate(Name=if_else(Name==team, "Team", Name)) %>%
@@ -26,7 +26,7 @@ get_all_stats <- function(gs, team) {
     mutate(X=as_factor(if_else(!is.na(Number), paste(Number, Name), Name))) %>%
     nest(data=c(-X, -type))
   pitching_stats <- the_game_stats |> filter(Team==team) |>
-    select(about, when, any_of('scorecard_link'), vs, Pitcher_Stats) |> unnest(Pitcher_Stats) |>
+    select(about, when, any_of('scorecard_link'), Pitcher_Stats) |> unnest(Pitcher_Stats) |>
     mutate(type="Pitching") |>
     bind_rows(all_the_stats$Pitching %>% mutate(type="Pitching", about="Season")) %>%
     mutate(NumberX=replace_na(Number, -1)) %>% arrange(NumberX) %>% select(-NumberX) %>%
@@ -55,13 +55,9 @@ get_all_stats <- function(gs, team) {
 }
 
 get_contact_rates <- function(gs, team) {
-  gg <- map_dfr(seq_len(nrow(gs)), function(idx) {
-    g <- gs[idx,] |> select(code, lineup, plays) |> flatten()
-    out <- g$plays |> mutate(Team=names(g$lineup)[Side+1]) |> filter(Team==team) |>
-      rename(Number=Batter)
-    nsx <- rr[[team]] |> select(Number, Name)
-    out <- out |>  left_join(nsx, by="Number") |> mutate(code=g$code)
-  })
+  gg <- gs |> select(code, game) |> unnest(game) |> filter(Team==team) |>
+    mutate(Plays=map2(Plays, Lineup, left_join, by=c("Lineup"))) |>
+    select(code, Plays) |> unnest(Plays) |> select(code, Number, Name, everything())
   gg |> count(Name, Contact) |> pivot_wider(names_from=Contact, values_from=n, values_fill = 0) |>
     select(Name, BBHB, K, Soft, Hard) |>
     mutate(Contact=Soft+Hard, AB=Soft+Hard+K, 'Contact/AB'=(Hard+Soft)/AB, 'Hard/AB'=Hard/AB, 'Hard/Contact'=Hard/(Soft+Hard)) |>
