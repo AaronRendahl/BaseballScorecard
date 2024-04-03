21504e152377b5258d2f5dc151bc0f16a3e13e48
Aaron Rendahl <rend0020@umn.edu>
2024-04-03 16:27:57 -0500

    new prep game function

--- R/read_data.R
+++ R/read_data.R
@@ -40,6 +40,41 @@ prep_game <- function(game) {
   game
 }
 
+prep_game2 <- function(game) {
+  stopifnot(is_tibble(game) && nrow(game)==1)
+
+  g <- game$game[[1]]
+
+  lx <- local({
+    p1 <- g |> select(Side, Plays) |> unnest(Plays) |>
+      arrange(Side, Row) |> select(Side, Number=Pitcher) |> unique() |>
+      mutate(Order=1:n(), .by=Side) |> mutate(Side=3-Side)
+    g |> select(Side, Team, Lineup) |> unnest(Lineup) |>
+      full_join(p1, by=c("Side", "Number")) |>
+      mutate(Player=sprintf("%d%3d", Side, Number)) |>
+      arrange(Side, Lineup, Order)
+  })
+
+  px <- g |> select(Side, Plays) |> unnest(Plays) |>
+    arrange(Inning, Side, Row) |> mutate(Row=1:n()) |>
+    left_join(lx |> select(Side, Pitcher=Number, Pitcher_Code=Player) |> mutate(Side=3-Side),
+              by=c("Side", "Pitcher")) |>
+    left_join(lx |> select(Side, Lineup, Batter_Code=Player),
+              by=c("Side", "Lineup")) |>
+    select(-Pitcher, -Lineup) |>
+    rename(Pitcher=Pitcher_Code, Batter=Batter_Code) |>
+    select(Row, Inning, Side, Batter, Pitcher, everything())
+
+  lx <- lx |> select(Player, Team, Number, Name, Lineup, Order)
+
+  game <- as.list(game)
+  game$teams <- game$game[[1]]$Team
+  game$plays <- px
+  game$lineup <- lx
+  game$game <- NULL
+  game
+}
+
 readgames <- function(dir=".", gamecode="^Game_([0-9a-z]+)\\.xlsx$",
                       files=list.files(path="game_data", pattern=gamecode, full.names=TRUE),
                       codes=str_replace(basename(files), gamecode, "\\1"),
