226b01ffa68e99663c70873859c7e1fec8c8520e
Aaron Rendahl <rend0020@umn.edu>
2024-04-05 17:52:32 -0500

    create version of plays with runner movements on separate lines

--- R/runners.R
+++ R/runners.R
@@ -1,30 +1,25 @@
-addBatterID <- function(x) {
-  x |> arrange(Inning, Side, Row) |>
-    mutate(x=Lineup!=lag(Lineup, default=0), .by=c(Side, Inning)) |>
-    mutate(BatterID=cumsum(x), .after=Lineup) |> select(-x)
-}
-
-find_runners <- function(plays, key.Base) {
+# need to have added "Base" to plays to specify how many bases the batter initially made it to
+find_runners <- function(plays, pattern.out="^X") {
   rx <- plays |> filter(!is.na(B1)) |>
-    select(RunnerID=BatterID, Runner=Lineup, Pitcher, Inning, Side, B1, B2, B3, B4) |>
-    # how many bases did they get on their at bat
-    left_join(key.Base, by="B1") |>
-    mutate(Base=case_when(!is.na(Base) ~ Base, !is.na(B1) ~ 1L, TRUE ~0L)) |>
-    select(-B1) |>
+    select(RunnerID=BatterID, Runner=Lineup, Pitcher, Row, Inning, Side, Base, B2, B3, B4) |>
     # now pivot longer and remove any rows that they reached due to their at bat
     pivot_longer(c(B2, B3, B4), names_to="B") |>
     mutate(B=as.integer(str_sub(B, 2, 2))) |>
     mutate(value=if_else(B==Base & B > 1, ".", value)) |>
+    select(-Base) |>
+    rename(Base="B") |>
     # now backfill for any that they moved two bases on the same play
     group_by(RunnerID) |>
     fill(value, .direction="up") |>
     ungroup() |>
     # and now remove bases they didn't get to
     filter(!is.na(value) & value!=".") |>
-    select(-Base) |>
-    # now can split the value into How/When/Note
-    mutate(value=value |> str_replace("^([0-9])", "P\\1")) |>
-    separate_wider_regex(value, c(How="[A-Z]+", Lineup="[0-9]+", Note=".+"), too_few="align_start") |>
+    # now can split the value into Out/How/When/Note
+    # if how not specified, assume it was part of the play
+    mutate(Out=str_detect(value, pattern.out)*1L,
+           value=str_remove(value, pattern.out),
+           value=str_replace(value, "^([0-9])", "P\\1")) |>
+    separate_wider_regex(value, c(How="[^0-9]*", Lineup="[0-9]+", Note=".*"), too_few="align_start") |>
     mutate(Lineup=as.integer(Lineup)) |>
     mutate(idx=1:n())
 
@@ -56,7 +51,7 @@ find_runners <- function(plays, key.Base) {
                              TRUE ~ 0),                        # before this specific play
            .after=BatterID) |>
     # if know when made it to a previous base, then everything after that must be after that
-    arrange(RunnerID, B) |>
+    arrange(RunnerID, Base) |>
     group_by(RunnerID) |>
     fill(BatterID) |> fill(Lineup) |>
     ungroup() |>
@@ -68,4 +63,36 @@ find_runners <- function(plays, key.Base) {
       if(nrow(tmp)) {print(tmp); stop("became runner before batted!")}
       x
     })()
+}
+
+make_plays <- function(g,
+                       pattern.out="^X",
+                       noPlay = "_",
+                       key.Base = key |> filter(Base>1) |> select(B1=Outcome, Base)) {
+  stopifnot(is_tibble(g) & nrow(g)==1)
+  p2 <- g$game[[1]] |> select(Side, Plays) |> unnest(Plays)
+
+  p2 |>
+    ## add BatterID
+    arrange(Inning, Side, Row) |>
+    mutate(x=Lineup!=lag(Lineup, default=0), .by=c(Side, Inning)) |>
+    mutate(BatterID=cumsum(x), .after=Lineup) |> select(-x) |>
+    ## rename
+    rename(NumOut=Out, onPitch=PitchesAtBat) |>
+    ## add Base
+    left_join(key.Base, by="B1") |>
+    mutate(
+      Base=case_when(!is.na(Base) ~ Base, !is.na(B1) ~ 1L, TRUE ~0L),
+      Base=if_else(Play %in% noPlay, NA, Base)) |>
+    ## add Out
+    mutate(Out=(Base==0)*1L)
+
+  rx <- find_runners(px, pattern.out)
+
+  bind_rows(rx, px) |>
+    arrange(BatterID, onPitch) |>
+    select(Side, Row, Inning, BatterID, onPitch, Lineup, Pitcher,
+           Runner, RunnerID,
+           Balls, Strikes, Fouls,
+           Play, B1, How, Note, Base, Out)
 }
