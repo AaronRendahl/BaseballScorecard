f71ab134abf77a46e31ccc3b9405b263e335bcd8
Aaron Rendahl <rend0020@umn.edu>
2024-05-20 16:57:15 -0500

    add new version of game_stats

--- R/stats_game.R
+++ R/stats_game.R
@@ -146,3 +146,22 @@ game_stats <- function(g, team) {
 
   c(list(header=header), out)
 }
+
+game_stats2 <- function(g, team, stats_file, calculations, roles) {
+  stopifnot(is_tibble(g) & nrow(g)==1)
+  teams <- g |> select(code, game) |> unnest(game) |> pull(Team)
+  stopifnot(team %in% teams)
+  vs <- setdiff(teams, team)
+
+  header <- sprintf("%s, %s", paste(teams, collapse=" @ "), MDY_format(g$when))
+
+  if(!is.null(g$scorecard_link)) {
+    link <- g$scorecard_link
+    names(link) <- "LINK TO SCORECARD"
+    class(link) <- "hyperlink"
+    header <- list(header, link)
+  }
+
+  out <- make_stats_from_file(g, header, stats_file, calculations, roles, team, vs)
+  out
+}
