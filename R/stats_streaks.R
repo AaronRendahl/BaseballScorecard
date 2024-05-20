2dcf7535885cdfcdf81fbcfdca2f67476eb1bc19
Aaron Rendahl <rend0020@umn.edu>
2024-05-20 16:57:29 -0500

    revise streaks to use new code and to be more general

--- R/stats_streaks.R
+++ R/stats_streaks.R
@@ -1,37 +1,51 @@
-batting_streaks <- function(gs, team, roll_num=4) {
+streaks <- function(gs, team, roll_num=4, calcs,
+                            var="SLG + OBPE + notK/PA:\nBatting Sum",
+                            title="Summary",
+                            full_title,
+                            role=c("Batter/Runner", "Pitcher")) {
   stopifnot(is_tibble(gs))
+  role <- match.arg(role)
 
-  lx <- gs |> select(code, game) |> unnest(game) |> select(code, Side, Team, Lineup) |> unnest(Lineup)
-  lx_Batter <- lx |> select(code, Side, Team, Number, Name)
+  lx <- gs |> select(code, game) |> unnest(game) |> select(code, Side, Team, Lineup) |> unnest(Lineup) |>
+    select(code, Side, Team, Number, Name)
 
   cs2 <- counting_stats_all(gs)
-  cn <- cs2$names
 
-  cs <- cs2$stats |> mutate(idx=as.integer(factor(code)), .before=1) |>
-    rename(Number=Batter) |> left_join(lx_Batter, by=c('code', 'Side', 'Number')) |>
-    filter(Team==team)
+  if(role=="Batter/Runner") {
+    cs <- cs2$stats |> mutate(idx=as.integer(factor(code)), .before=1) |>
+      rename(Number=Batter) |> left_join(lx, by=c('code', 'Side', 'Number')) |>
+      filter(Team==team)
+  } else {
+    lx$Side <- 3 - lx$Side
+    cs <- cs2$stats |> mutate(idx=as.integer(factor(code)), .before=1) |>
+      rename(Number=Pitcher) |> left_join(lx, by=c('code', 'Side', 'Number')) |>
+      filter(Team==team)
+  }
+  cv <- get_calc_vars(var, calcs)
 
-  calcs <- batter_calculations[c("SLG", "OBPE", "K/PA", "SLG + OBPE + notK/PA:\nBatting Sum")]
-  names(calcs)[4] <- "y"
-  need <- c("TB", "AB", "H", "BB", "HBP", "ROE", "PA", "K")
-  remove <- c("Games", setdiff(c(names(calcs), need), "y"))
-
-  X.all <- calc_stats(cs, need, calcs, by="Name", total=c(Name="Team")) |> select(-all_of(remove))
+  X.all <- calc_stats(cs, cv$Counts, cv$Calculations, by="Name", total=c(Name="Team")) |>
+    select("Name", all_of(var)) |> rename(y=!!var)
   X.indall <- X.all |> filter(Name!="Team") |> arrange(desc(y)) |> mutate(Name=as_factor(Name))
   X.teamall <- X.all |> filter(Name=="Team") |> select(-Name)
 
-  csx1 <- cs |> select(idx, code, Name, all_of(need)) |>
-    summarize(across(all_of(need), \(x) sum(x, na.rm=TRUE)), .by=all_of(c("idx", "Name")))
-  csx2 <- csx1 |> mutate(across(all_of(need), \(x) zoo::rollsum(x, k=roll_num, fill=NA, align="right")), .by="Name") |>
-    filter(!is.na(TB))
-  X.team <- calc_stats(csx2 |> mutate(code=1), need, calcs, by="idx") |> select(-any_of(remove))
+  csx1 <- cs |> select(idx, code, Name, all_of(cv$Counts)) |>
+    summarize(across(all_of(cv$Counts), \(x) sum(x, na.rm=TRUE)), .by=all_of(c("idx", "Name")))
+  csx2 <- csx1 |> mutate(.x=1L) |> mutate(across(all_of(c(cv$Counts, ".x")), \(x) zoo::rollsum(x, k=roll_num, fill=NA, align="right")), .by="Name") |>
+    filter(!is.na(.x)) |> select(-.x)
+  X.team <- calc_stats(csx2 |> mutate(code=1), cv$Counts, cv$Calculations, by="idx", total=NA) |>
+    select(idx, all_of(var)) |> rename(y=!!var)
 
-  for(n in names(calcs)) { csx2[[n]] <- with(csx2, eval(calcs[[n]])) }
-  X.ind <- csx2 |> select(-any_of(remove)) |> mutate(Name=factor(Name, levels=levels(X.indall$Name)))
+  for(n in names(cv$Calculations)) { csx2[[n]] <- with(csx2, eval(cv$Calculations[[n]])) }
+  X.ind <- csx2 |> select(idx, Name, all_of(var)) |> rename(y=!!var) |> mutate(Name=factor(Name, levels=levels(X.indall$Name)))
 
   coli <- "black"
   colt <- "gray60"
   subt <- "Black shows player, gray shows team average; straight lines are season averages"
+
+  if(missing(full_title)) {
+    roll_txt <- if(roll_num > 1) sprintf(" (Average for last %d games)", roll_num) else ""
+    full_title <- sprintf("%s%s", title, roll_txt)
+  }
 
   ggplot(X.ind) + aes(idx, y) +
     facet_wrap(~Name, nrow=2) +
@@ -40,7 +54,7 @@ batting_streaks <- function(gs, team, roll_num=4) {
     geom_line(data=X.team, col=colt) + geom_point(data=X.team, fill=colt, pch=21) +
     geom_line(col=coli) + geom_point(fill=coli, pch=21) +
     xlab("Game Number") +
-    scale_x_continuous(breaks=1:10) + #seq(0, 40, by=10)) +
-    ggtitle(sprintf("Batting Summary (Average for last %d games)", roll_num), subtitle=subt) +
-    ylab("Batting Sum: SLG + OBPE + notK/PA")
+    scale_x_continuous() +
+    ggtitle(full_title, subtitle=subt) +
+    ylab(var)
 }
