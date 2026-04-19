4028eab47f03407f0a999b96e63027a218e48ae8
Aaron Rendahl <rend0020@umn.edu>
2026-04-19 10:20:54 -0500

    move makebox to own file

--- R/scorecard.R
+++ R/scorecard.R
@@ -49,7 +49,8 @@ scorecard <- function(game, file="_scorecard_tmp.pdf",
   #pitchsize       <- if(missing(game)) 0.12 else 0.10
   #pitchboxcolor   <- "gray30"
   #pitchslashcolor <- "gray30"
-
+  #basedotcolor <- "gray50"
+  #pitchcountsize <-  8
 
   teamnamesize   <- 14
   footertextsize <-  8; footertextcolor <- "gray20"
@@ -58,159 +59,7 @@ scorecard <- function(game, file="_scorecard_tmp.pdf",
   lineupnumsize  <- 14
   numbersize     <- 12; numbercolor     <- "gray50"
   leftlabelsize  <-  9; leftlabelcolor  <- "gray50"
-  pitchcountsize <-  8
 
-  #basedotcolor <- "gray50"
-
-  makebox <- function(ToBase=NA, count=c(0,0), pitchcount=NA, LastPitch=FALSE,
-                      out=NA, bybase, play=NA,
-                      basesize=0.13,
-                      pitchsize=if(blank) 0.12 else 0.10,
-                      blank=TRUE, top=FALSE,
-                      basedotcolor="gray50",
-                      pitchslashcolor="gray30",
-                      pitchboxcolor="gray30",
-                      count0=c(0,0)) {
-    basesize  <- unit(basesize,  "inches")
-    pitchsize <- unit(pitchsize, "inches")
-    basex <- unit(0.55, "npc")
-    basey <- unit(if(blank) 0.5 else 0.6, "npc")
-    xs <- (c(1, 2, 3, 1, 2) - 1) * pitchsize
-    ys <- unit(1, "npc") - (c(1, 1, 1, 2, 2) - 1) * pitchsize
-    pitchboxes <- do.call(gList, mapply(function(x, y) {
-      rectGrob(x = x, y = y,
-               width  = pitchsize,
-               height = pitchsize,
-               just = c("left","top"),
-               gp = gpar(fill = "white", lwd = 0.25, col = pitchboxcolor))
-    }, xs, ys, SIMPLIFY = FALSE))
-    fouls <- if(length(count) == 3 && count[3] > 0) {
-      xx <- pitchsize * (seq_len(count[3]) - 0.5)
-      yy <- unit(1, "npc") - pitchsize * 2.67
-      pointsGrob(x = xx, y=rep(yy, count[3]), pch = 19,
-                 size = unit(2, "pt"))
-    } else { NULL }
-    countX_start <- if(any(count0 > 0)) {
-      X <- rep(FALSE, 5)
-      X[seq_len(count0[1])] <- TRUE
-      X[seq_len(count0[2])+3] <- TRUE
-      segmentsGrob(x0=xs[X] + pitchsize*0.1,
-                   y0=ys[X] - pitchsize*0.5,
-                   x1=xs[X] + pitchsize*0.9,
-                   y1=ys[X] - pitchsize*0.5)
-    } else { NULL }
-    countX <- if(any(count[1:2] > 0)) {
-      X <- rep(FALSE, 5)
-      X[seq_len(count[1])+start_count[1]] <- TRUE
-      X[seq_len(count[2])+3+start_count[2]] <- TRUE
-      segmentsGrob(x0=xs[X],
-                   y0=ys[X] - pitchsize,
-                   x1=xs[X] + pitchsize,
-                   y1=ys[X])
-    } else { NULL }
-    box <- rectGrob(gp = gpar(lwd = 0.5))
-    if(top) {
-      box <- gList(box, segmentsGrob(x0 = 0, x1 = 1, y0 = 1, y1 = 1, gp=gpar(lwd = 2)))
-    }
-    xs <- basex + c(0, 1, 0, -1, 0) * basesize
-    ys <- unit(1, "npc") - basey + c(-1, 0, 1, 0, -1) * basesize
-    bases <- if(!is.na(ToBase)) { NULL } else {
-      pointsGrob(x = xs[1:4],
-                 y = ys[1:4],
-                 pch = 19, size = unit(0.25, "pt"), gp = gpar(col=basedotcolor))
-    }
-    lines <- if(is.na(ToBase)) { NULL } else {
-      ex <- ToBase - floor(ToBase + 0.01)  ## add 0.01, caution about floating point
-      to <- floor(ToBase + 0.01)
-      xx <- xs[seq_len(ToBase + 1)]
-      yy <- ys[seq_len(ToBase + 1)]
-      outbar <- NULL
-      if(ex > 0.1) {
-        xs2 <- c(0.25, -0.75, -0.25)[ToBase] * basesize + basex
-        ys2 <- c(0.75,  0.25, -0.75)[ToBase] * basesize + unit(1, "npc") - basey
-        xa <- xs2 + c(-1,  1,  1)[ToBase] * c(-1, 1) * 0.2 * basesize
-        ya <- ys2 + c(-1, -1,  1)[ToBase] * c(-1, 1) * 0.2 * basesize
-        outbar <- linesGrob(x = xa, y = ya, gp = gpar(lwd = 2))
-        xx <- unit.c(xx, xs2)
-        yy <- unit.c(yy, ys2)
-      }
-      gList(linesGrob(x = xx, y = yy, gp = gpar(col="black")),
-            outbar)
-    }
-    fill <- if(!is.na(ToBase) && ToBase == 4) {
-      polygonGrob(x = xs, y = ys, gp = gpar(fill = "gray50"))
-    } else { NULL }
-    # slash for pitches atbat/total
-    sh <- segmentsGrob(x0 = unit(0.5, "npc") + pitchsize * 1.5 - pitchsize * 3 / 16 * 1.5,
-                       x1 = unit(0.5, "npc") + pitchsize * 1.5 + pitchsize * 3 / 16 * 1.5,
-                       y0 = unit(1.0, "npc") - pitchsize * 1.5, # how tall is slash
-                       y1 = unit(1.0, "npc"),
-                       gp = gpar(col = pitchslashcolor, lwd = 0.25))
-    pitchnum <- if(any(is.na(pitchcount))) { NULL } else {
-      a2x <- (unit(1, "npc") + unit(0.5, "npc") + pitchsize*1.5)/2
-      a2y <-  unit(1, "npc") - pitchsize*3/4
-      a1 <- textGrob(pitchcount[1],
-                     x = unit(0.5, "npc") + pitchsize * (1.5 - 3 / 16 * 1.5),
-                     y = a2y,
-                     gp = gpar(fontsize=pitchcountsize), just = "right")
-      a2 <- textGrob(pitchcount[2],
-                     x = a2x, y = a2y,
-                     gp = gpar(fontsize = pitchcountsize))
-      bb <- if(!LastPitch) { NULL } else {
-        rectGrob(x = a2x, y = a2y,
-                 width = unit(1.5, "grobwidth", a2), height = unit(1.5, "grobheight", a2))
-      }
-      gList(a1, a2, bb)
-    }
-    out <- if(!is.na(out)) {
-      xx <- yy <- unit(1, "strheight", data = paste(out))
-      gList(textGrob(out, x = xx, y = yy),
-            circleGrob(x = xx, y = yy, r = xx * 0.85))
-    } else { NULL }
-    play <- if(is.na(play)) { NULL } else {
-      play <- str_replace_all(play, "-", "")
-      if(ToBase == 0) {
-        textGrob(play, basex, unit(1, "npc") - basey)
-      } else {
-        textGrob(play,
-                 # ## for in action spot
-                 # x=basex, y=unit(0.1,"npc"),
-                 ## for above B1 text
-                 x = unit(1, "npc"), y = unit(1, "npc") - basey, just = "right",
-                 gp = gpar(fontsize=8))
-      }
-    }
-    bybase <- if(missing(bybase)) { NULL } else {
-      textGrobNA <- function(label, x, y, ...) {
-        if(is.na(label)) return(NULL)
-        textGrob(label, x, y, ...)
-      }
-      yy <- unit(1, "strheight", data=bybase[1])
-      xx <- unit(1,  "strwidth", data=bybase[1])/2 + yy/3
-      gList(textGrobNA(bybase[1], x = unit(1,"npc") - xx,
-                       y = yy),
-                       # ## to move up above action
-                       # y = yy + unit(0.2, "npc")),
-            textGrobNA(bybase[2], x = basex + basesize,
-                       y=unit(1,"npc") - basey + basesize, gp = gpar(fontsize=8)),
-            textGrobNA(bybase[3], x = basex - basesize,
-                       y=unit(1,"npc") - basey + basesize, gp = gpar(fontsize=8)),
-            textGrobNA(bybase[4], x = basex - basesize,
-                       y=unit(1,"npc") - basey - basesize, gp = gpar(fontsize=8))
-      )
-    }
-    action <- if(!blank) { NULL } else {
-      segmentsGrob(x0 = unit(0.25, "npc"),
-                   x1 = unit(1.00, "npc"),
-                   y0 = unit(0.20, "npc"),
-                   y1 = unit(0.20, "npc"),
-                   gp = gpar(col=pitchslashcolor, lwd=0.25))
-    }
-    gTree(children=gList(bases, lines, fill, action,
-                         pitchboxes, countX_start, countX, fouls,
-                         play, out, bybase,
-                         sh, pitchnum, box))
-  }
 
   left <- function(lineup, nrow) {
     gf <- frameGrob(layout = grid.layout(ncol = 1, nrow = nrow))
@@ -233,18 +82,18 @@ scorecard <- function(game, file="_scorecard_tmp.pdf",
       s2 <- textGrob(i, x = 0, y = 0.85, just = c("left", "top"),
                      gp = gpar(fontsize = lineupnumsize))
       s3a <- textGrob("#", x = 0.15, y = 0.8, just = c("center", "center"),
-                     gp = gpar(fontsize = numbersize, col = numbercolor))
+                      gp = gpar(fontsize = numbersize, col = numbercolor))
       s3b <- textGrob("#", x = 0.15, y = 0.5, just = c("center", "center"),
-                     gp = gpar(fontsize = numbersize, col = numbercolor))
+                      gp = gpar(fontsize = numbersize, col = numbercolor))
       s3c <- textGrob("#", x = 0.15, y = 0.2, just = c("center", "center"),
-                     gp = gpar(fontsize = numbersize, col = numbercolor))
+                      gp = gpar(fontsize = numbersize, col = numbercolor))
       ss <- gTree(children = gList(s1, s2, s3a, s3b, s3c))
       ## FIX 1
       gf <- placeGrob(gf, row = i, col = 1, grob = ss)
     }
     gf <- placeGrob(gf, row = players, col = 1,
                     grob = segmentsGrob(x0 = 0, x1 = 1, y0 = 0, y1 = 0,
-                                      gp = gpar(lwd = 0.5)))
+                                        gp = gpar(lwd = 0.5)))
     gf
   }
 
