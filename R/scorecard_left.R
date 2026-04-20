8a45e8ea1a57d8198376d043f8ec0a0af6cacb59
Aaron Rendahl <rend0020@umn.edu>
2026-04-19 20:58:42 -0500

    put multiple names using Sub column

--- R/scorecard_left.R
+++ R/scorecard_left.R
@@ -6,35 +6,31 @@ left <- function(lineup = NULL,
                  numbercolor = "gray50") {
   gf <- frameGrob(layout = grid.layout(ncol = 1, nrow = nrow))
   if(blank) {
-    players <- nrow
-  } else {
-    lineup <- lineup |> filter(!is.na(Number)) |> mutate(Name=replace_na(Name, ""))
-    players <- max(lineup$Lineup)
-    for(i in 1:nrow(lineup)) {
-      s1 <- textGrob(lineup$Number[i], x = 0.25, y = 0.1, just = c("left", "bottom"))
-      if(!("Name" %in% names(lineup))) {s2 <- NULL} else {
-        s2 <- textGrob(lineup[["Name"]][i], x = 0.25, y = 0.85, just=c("left", "top"))
+    for(i in 1:nrow) {
+      for(y in c(0.2, 0.5, 0.8)) {
+        s3 <- textGrob("#", x = 0.15, y = y, just = c("center", "center"),
+                        gp = gpar(fontsize = numbersize, col = numbercolor))
+        gf <- placeGrob(gf, row = i, col = 1, grob = s3)
       }
-      ss <- gTree(children = gList(s1, s2))
-      gf <- placeGrob(gf, row = lineup$Lineup[i], col = 1, grob = ss)
+    }
+  } else {
+    nrow <- max(lineup$Lineup)
+    lineup <- lineup |> filter(!is.na(Number)) |> mutate(Name=replace_na(Name, "")) |>
+      mutate(NN=sprintf("#%d %s", Number, Name))
+    if(!"Sub" %in% names(lineup)) lineup$Sub <- 1
+    for(i in 1:nrow(lineup)) {
+      s1 <- textGrob(lineup$NN[i], x = 0.15, y = 1-0.3*lineup$Sub[i], just = c("left", "bottom"))
+      gf <- placeGrob(gf, row = lineup$Lineup[i], col = 1, grob = s1)
     }
   }
-  for(i in 1:players) {
+  for(i in 1:nrow) {
     s1 <- segmentsGrob(x0 = 0, x1 = 1, y0 = 1, y1 = 1, gp = gpar(lwd=0.5))
     s2 <- textGrob(i, x = 0, y = 0.85, just = c("left", "top"),
                    gp = gpar(fontsize = lineupnumsize))
-    s3a <- textGrob("#", x = 0.15, y = 0.8, just = c("center", "center"),
-                    gp = gpar(fontsize = numbersize, col = numbercolor))
-    s3b <- textGrob("#", x = 0.15, y = 0.5, just = c("center", "center"),
-                    gp = gpar(fontsize = numbersize, col = numbercolor))
-    s3c <- textGrob("#", x = 0.15, y = 0.2, just = c("center", "center"),
-                    gp = gpar(fontsize = numbersize, col = numbercolor))
-    ss <- gTree(children = gList(s1, s2, s3a, s3b, s3c))
-    ## FIX 1
-    gf <- placeGrob(gf, row = i, col = 1, grob = ss)
+    gf <- placeGrob(gf, row = i, col = 1, grob = gTree(children = gList(s1, s2)))
   }
-  gf <- placeGrob(gf, row = players, col = 1,
-                  grob = segmentsGrob(x0 = 0, x1 = 1, y0 = 0, y1 = 0,
-                                      gp = gpar(lwd = 0.5)))
+  # gf <- placeGrob(gf, row = nrow, col = 1,
+  #                 grob = segmentsGrob(x0 = 0, x1 = 1, y0 = 0, y1 = 0,
+  #                                     gp = gpar(lwd = 0.5)))
   gf
 }
